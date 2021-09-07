2f9794ab12a1a4bf3dd8e0ebedd8d8cd2f8a6ea1
SantiSalmen <santisalmen@gmail.com>
2021-09-07 19:59:28 -0300

    un comentario

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using Jugadores;
 
-namespace _2._1
+namespace clase2
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("New word");
+        { 
+          Jugador jug1 = new Jugador("Pepe", 30, true);
+          Jugador jug2 = new Jugador("Pepe1", 20);
+          Jugador jug3 = new Jugador("Pepe2", 30, true);
+
+          Console.WriteLine(jug2.IsActivo());
         }
     }
 }
